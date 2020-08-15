c47d48e792e1b3d3c98faebb2dc0cbb671b08fbf
Charlie Hornsby <charlie.hornsby@hotmail.co.uk>
2020-08-15 10:22:30 +0300

    Add basic test for Interpreter

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,12 +1,14 @@
 use crate::command::Command;
 use crate::program::Program;
 
+/// A Brainfuck program interpreter with its own mutable memory
 pub struct Interpreter {
     memory: [u8; 30_000],
     memory_pointer: usize,
 }
 
 impl Interpreter {
+    /// Initialises the interpreter with the classic 30 000 cells of memory
     pub fn new() -> Self {
         Self {
             memory: [0; 30_000],
@@ -14,6 +16,7 @@ impl Interpreter {
         }
     }
 
+    /// Runs the given valid Brainfuck program to completion
     pub fn run(&mut self, program: &Program) {
         let mut command_pointer = 0;
 
@@ -45,5 +48,32 @@ impl Interpreter {
 impl Default for Interpreter {
     fn default() -> Self {
         Interpreter::new()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::convert::TryFrom;
+
+    #[test]
+    fn run_valid_program() {
+        let input = b"++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]";
+        let program = Program::try_from(input.to_vec().as_slice()).unwrap();
+
+        let mut interpreter = Interpreter::new();
+        interpreter.run(&program);
+
+        assert_eq!(interpreter.memory_pointer, 0);
+        assert_eq!(interpreter.memory[0], 0);
+        assert_eq!(interpreter.memory[1], 0);
+        assert_eq!(interpreter.memory[2], 72);
+        assert_eq!(interpreter.memory[3], 104);
+        assert_eq!(interpreter.memory[4], 88);
+        assert_eq!(interpreter.memory[5], 32);
+        assert_eq!(interpreter.memory[6], 8);
+        assert_eq!(interpreter.memory[7], 0);
+        assert_eq!(interpreter.memory[8], 0);
+        assert_eq!(interpreter.memory[9], 0);
     }
 }
