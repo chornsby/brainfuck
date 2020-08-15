4867737fb6040654da21c6a3cb0a38bfea14c8d1
Charlie Hornsby <charlie.hornsby@hotmail.co.uk>
2020-08-15 10:02:59 +0300

    Add basic tests for program parsing

--- src/program.rs
+++ src/program.rs
@@ -7,6 +7,7 @@ use crate::command::Command;
 ///
 /// To simplify looping logic in the interpreter we are caching the positions
 /// of all BeginLoop and EndLoop pairs.
+#[derive(Debug)]
 pub struct Program {
     pub commands: Vec<Command>,
     pub loop_map: HashMap<usize, usize>,
@@ -15,6 +16,8 @@ pub struct Program {
 impl TryFrom<&[u8]> for Program {
     type Error = &'static str;
 
+    /// Tries to parse all commands and cache loop jump indices for a slice
+    /// of bytes
     fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
         let mut commands: Vec<Command> = vec![];
         let mut loop_stack: Vec<usize> = vec![];
@@ -45,5 +48,56 @@ impl TryFrom<&[u8]> for Program {
         } else {
             Err("Unmatched [")
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn valid_program() {
+        let input = b"[->+<]";
+        let result = Program::try_from(input.iter().as_slice());
+        let program = result.unwrap();
+
+        assert_eq!(
+            program.commands,
+            vec![
+                Command::BeginLoop,
+                Command::Decrement,
+                Command::StepForward,
+                Command::Increment,
+                Command::StepBackward,
+                Command::EndLoop
+            ]
+        );
+        assert_eq!(program.loop_map.len(), 2);
+        assert_eq!(program.loop_map[&0], 5);
+        assert_eq!(program.loop_map[&5], 0);
+    }
+
+    #[test]
+    fn missing_end_loop() {
+        let input = b"[";
+        let result = Program::try_from(input.iter().as_slice());
+
+        assert_eq!(result.unwrap_err(), "Unmatched [");
+    }
+
+    #[test]
+    fn missing_begin_loop() {
+        let input = b"]";
+        let result = Program::try_from(input.iter().as_slice());
+
+        assert_eq!(result.unwrap_err(), "Unmatched ]");
+    }
+
+    #[test]
+    fn invalid_loop_nesting() {
+        let input = b"[]][";
+        let result = Program::try_from(input.iter().as_slice());
+
+        assert_eq!(result.unwrap_err(), "Unmatched ]");
     }
 }
